68802df9ee3775f16ec635a28e9347c562ee6424
whojan <whojan@gmail.com>
2024-09-03 18:57:55 +0300

    Update game_control.py

--- game_control.py
+++ game_control.py
@@ -1,46 +1,20 @@
-# Arda Mavi
-from pynput.mouse import Button, Controller as Mouse
-from pynput.keyboard import Controller as Keyboard
+import time
+import pyautogui
+import os
 
-# For encoding keyboard keys:
-def get_keys():
-    return [
-        '\t', '\n', '\r', ' ', '!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`',
-        'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '{', '|', '}', '~', 'accept', 'add', 'alt', 'altleft', 'altright', 'apps', 'backspace',
-        'browserback', 'browserfavorites', 'browserforward', 'browserhome', 'browserrefresh', 'browsersearch', 'browserstop', 'capslock', 'clear', 'convert', 'ctrl', 'ctrlleft', 'ctrlright', 'decimal', 'del', 'delete', 'divide', 'down',
-        'end', 'enter', 'esc', 'escape', 'execute', 'f1', 'f10', 'f11', 'f12', 'f13', 'f14', 'f15', 'f16', 'f17', 'f18', 'f19', 'f2', 'f20', 'f21', 'f22', 'f23', 'f24', 'f3', 'f4', 'f5', 'f6', 'f7', 'f8', 'f9', 'final', 'fn', 'hanguel',
-        'hangul', 'hanja', 'help', 'home', 'insert', 'junja', 'kana', 'kanji', 'launchapp1', 'launchapp2', 'launchmail', 'launchmediaselect', 'left', 'modechange', 'multiply', 'nexttrack', 'nonconvert', 'num0', 'num1', 'num2', 'num3', 'num4',
-        'num5', 'num6', 'num7', 'num8', 'num9', 'numlock', 'pagedown', 'pageup', 'pause', 'pgdn', 'pgup', 'playpause', 'prevtrack', 'print', 'printscreen', 'prntscrn', 'prtsc', 'prtscr', 'return', 'right', 'scrolllock', 'select', 'separator',
-        'shift', 'shiftleft', 'shiftright', 'sleep', 'space', 'stop', 'subtract', 'tab', 'up', 'volumedown', 'volumemute', 'volumeup', 'win', 'winleft', 'winright', 'yen', 'command', 'option', 'optionleft', 'optionright'
-    ]
+def capture_screen(filename):
+    screenshot = pyautogui.screenshot()
+    screenshot.save(filename)
 
-def get_key(id):
-    keys = get_keys()
-    return keys[id] if 0 <= id < len(keys) else None
+def main():
+    save_path = 'Data/Screenshots/'
+    if not os.path.exists(save_path):
+        os.makedirs(save_path)
 
-def get_id(key):
-    keys = get_keys()
-    return keys.index(key) if key in keys else -1
+    while True:
+        timestamp = int(time.time())
+        capture_screen(os.path.join(save_path, f'{timestamp}.png'))
+        time.sleep(1)  # Capture every second
 
-keyboard = Keyboard()
-mouse = Mouse()
-
-# Mouse:
-def move(x, y):
-    mouse.position = (x, y)
-
-def scroll(dx, dy):
-    mouse.scroll(dx, dy)
-
-def click(x, y):
-    mouse.position = (x, y)  # Ensure mouse is at the correct position before clicking
-    mouse.press(Button.left)
-    mouse.release(Button.left)
-
-# Keyboard:
-def press(key):
-    keyboard.press(key)
-
-def release(key):
-    keyboard.release(key)
-
+if __name__ == '__main__':
+    main()

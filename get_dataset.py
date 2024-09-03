1645ce5b33701af655f17bfcb77fa30386689265
whojan <whojan@gmail.com>
2024-09-03 18:20:27 +0300

    Update get_dataset.py
    get_img Fonksiyonu:
    imresize yerine scipy.ndimage.zoom kullanarak resimleri yeniden boyutlandırdım. zoom fonksiyonu daha iyi bir interpolasyon sağlar.
    save_img Fonksiyonu:
    imsave yerine imageio.imwrite kullanarak resmi kaydettim, çünkü scipy.misc.imsave artık önerilmemektedir.
    get_dataset Fonksiyonu:
    Etiketlerin encodini label_dict kullanarak yapıyorum. Bu, etiketlerin indekslerinin daha yönetilebilir bir şekilde saklanmasını sağlar.
    Dosya bulunamadığında FileNotFoundError yakaladım.

--- get_dataset.py
+++ get_dataset.py
@@ -1,18 +1,19 @@
 # Arda Mavi
 import os
 import numpy as np
-from keras.utils import to_categorical
-from scipy.misc import imread, imresize, imsave
+from tensorflow.keras.utils import to_categorical
+from scipy.ndimage import imread, zoom
 from sklearn.model_selection import train_test_split
 
 def get_img(data_path):
     # Getting image array from path:
     img = imread(data_path)
-    img = imresize(img, (150, 150, 3))
+    img = zoom(img, (150/img.shape[0], 150/img.shape[1], 1), order=3)  # Resize with interpolation
     return img
 
 def save_img(img, path):
-    imsave(path, img)
+    from imageio import imwrite
+    imwrite(path, img)
     return
 
 def get_dataset(dataset_path='Data/Train_Data'):
@@ -20,28 +21,30 @@ def get_dataset(dataset_path='Data/Train_Data'):
     try:
         X = np.load('Data/npy_train_data/X.npy')
         Y = np.load('Data/npy_train_data/Y.npy')
-    except:
-        labels = os.listdir(dataset_path) # Geting labels
+    except FileNotFoundError:
+        labels = os.listdir(dataset_path)  # Get labels
         X = []
         Y = []
-        count_categori = [-1,''] # For encode labels
+        count_categori = -1
+        label_dict = {}  # For encoding labels
         for label in labels:
-            datas_path = dataset_path+'/'+label
+            datas_path = os.path.join(dataset_path, label)
             for data in os.listdir(datas_path):
-                img = get_img(datas_path+'/'+data)
+                img = get_img(os.path.join(datas_path, data))
                 X.append(img)
-                # For encode labels:
-                if data != count_categori[1]:
-                    count_categori[0] += 1
-                    count_categori[1] = data.split(',')
-                Y.append(count_categori[0])
-        # Create dateset:
-        X = np.array(X).astype('float32')/255.
+                # For encoding labels:
+                if label not in label_dict:
+                    count_categori += 1
+                    label_dict[label] = count_categori
+                Y.append(label_dict[label])
+        # Create dataset:
+        X = np.array(X).astype('float32') / 255.
         Y = np.array(Y).astype('float32')
-        Y = to_categorical(Y, count_categori[0]+1)
+        Y = to_categorical(Y, count_categori + 1)
         if not os.path.exists('Data/npy_train_data/'):
             os.makedirs('Data/npy_train_data/')
         np.save('Data/npy_train_data/X.npy', X)
         np.save('Data/npy_train_data/Y.npy', Y)
     X, X_test, Y, Y_test = train_test_split(X, Y, test_size=0.1, random_state=42)
     return X, X_test, Y, Y_test
+

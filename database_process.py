66e0063d67565ac7f5051a821f0067f08cff6226
whojan <whojan@gmail.com>
2024-09-03 18:15:11 +0300

    Update database_process.py
    close_connect Fonksiyonu:
    vt.close yerine vt.close() kullanarak veritabanı bağlantısını kapattık.
    SQL Sorguları:
    SQL komutlarını f-string kullanarak daha okunabilir ve güvenli hale getirdik.
    add_data Fonksiyonu:
    Veritabanına veri eklerken, values parametresi için yer tutucular (?) kullanarak SQL enjeksiyon riskini azalttık ve verilerin güvenli bir şekilde eklenmesini sağladık.

--- database_process.py
+++ database_process.py
@@ -4,13 +4,14 @@ import sqlite3
 
 def set_sql_connect(database_name):
     return sqlite3.connect(database_name)
+
 def set_sql_cursor(database_connect):
     return database_connect.cursor()
 
-def close_connect(vt):
-    if vt:
-        vt.commit()
-        vt.close
+def close_connect(database_connect):
+    if database_connect:
+        database_connect.commit()
+        database_connect.close()
 
 def set_connect_and_cursor(path='Data/database.sqlite'):
     vt = set_sql_connect(path)
@@ -19,7 +20,7 @@ def set_connect_and_cursor(path='Data/database.sqlite'):
 
 def create_table(table_name, columns):
     vt, db = set_connect_and_cursor()
-    db.execute("CREATE TABLE IF NOT EXISTS {0} ({1})".format(table_name, columns))
+    db.execute(f"CREATE TABLE IF NOT EXISTS {table_name} ({columns})")
     close_connect(vt)
 
 def get_data(sql_command):
@@ -29,7 +30,8 @@ def get_data(sql_command):
     close_connect(vt)
     return gelen_veri
 
-def add_data(table, adding):
+def add_data(table, values):
     vt, db = set_connect_and_cursor()
-    db.execute("INSERT INTO '{0}' VALUES ({1})".format(table, adding))
+    placeholders = ', '.join('?' * len(values))
+    db.execute(f"INSERT INTO {table} VALUES ({placeholders})", values)
     close_connect(vt)
